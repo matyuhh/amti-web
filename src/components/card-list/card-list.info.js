7b73c194aa02529319c5b16c5d032d43e98d61bf
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-28 09:27:20 -0300

    added new web to portfolio

--- src/components/card-list/card-list.info.js
+++ src/components/card-list/card-list.info.js
@@ -1,6 +1,7 @@
 import ClothImage from '../../assets/cloth-ecommerce.PNG'
 import PokedexImage from '../../assets/pokedex-for-oldies.PNG'
 import YelpCampImage from '../../assets/yelp-camp.PNG'
+import SolidityImage from '../../assets/solidity.PNG'
 
 
 export const cardInfo = [
@@ -8,7 +9,7 @@ export const cardInfo = [
         id: 1,
         image: ClothImage,
         title: "Cloth Ecommerce",
-        description: 'Hecho con ReactJS para el curso de Andre Naegoie. Uso de Redux, ReduxThunk, ReduxSaga y Stripe.',
+        description: 'Hecho con ReactJS usando Redux, ReduxThunk, ReduxSaga y Stripe.',
         url: 'https://cloth-ecommerce-amti.herokuapp.com/'
     },
     {
@@ -24,6 +25,14 @@ export const cardInfo = [
         title: "YelpCamp",
         description: 'Webapp diseñada en NodeJS, Express y MongoDB para el curso de Colt Steele. Uso de Bootstrap y varias tecnologías/librerías adicionales.',
         url: '/'
+    },
+    {
+        id: 4,
+        image: SolidityImage,
+        title: "CryptoFunding",
+        description: 'Diseñada con NextJS, web3 y Solidity. Pseudoclon de Kickstarter pero con el uso de  Ethereum.',
+        url: 'https://cryptofunding-3c2diuwpu-matyuhh.vercel.app/'
     }
 
 ]
+
